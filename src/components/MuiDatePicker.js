54dbedaa068c8e3f372a52c2f6c38860def786f7
Finarfin32 <91962071+Finarfin32@users.noreply.github.com>
2022-06-26 15:34:47 +0200

    MUIDatePicker
    MUIDatePicker testing.
    Use MUI
    Show date after submit.

--- src/components/MuiDatePicker.js
+++ src/components/MuiDatePicker.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
@@ -7,23 +8,29 @@ import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
 function MuiDatePicker() {
   const [value, setValue] = React.useState(new Date("2014-08-18T21:11:54"));
-
+  console.log(value);
   const handleChange = (newValue) => {
     setValue(newValue);
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Stack spacing={3}>
-        <DesktopDatePicker
-          label="Date desktop"
-          inputFormat="MM/dd/yyyy"
-          value={value}
-          onChange={handleChange}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </Stack>
-    </LocalizationProvider>
+    <form>
+      <label>
+        Wpisz datę:
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Stack spacing={3}>
+            <DesktopDatePicker
+              label="Date desktop"
+              inputFormat="MM/dd/yyyy"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </Stack>
+          <button onClick={() => alert(value.toString())}>Data</button>
+        </LocalizationProvider>
+      </label>
+    </form>
   );
 }
 
