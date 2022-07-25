9d6ef9ecb6e4816d2928c9b7a3f43910d6480dbc
Finarfin32 <91962071+Finarfin32@users.noreply.github.com>
2022-07-25 09:42:47 +0200

    Search Bar
    Search Bar in React JS with MUI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import { React } from "react";
+import { React, useState } from "react";
 import "./App.css";
 // import Flex from "./components/Flex";
 // import Fetch from "./components/Fetch";
 // import SimpleAccordion from "./components/AccordionMUI";
-import Check from "./components/Check";
+// import Check from "./components/Check";
 // import Color from "./components/Color";
+import TextField from "@mui/material/TextField";
+import List from "./components/List.js";
+
 function App() {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <Fetch /> */}
         {/* <SimpleAccordion /> */}
         {/* <Flex /> */}
-        <Check />
+        {/* <Check /> */}
         {/* <Color /> */}
+
+        <div className="main">
+          <h1>Wyszukiwarka</h1>
+          <div className="search">
+            <TextField
+              id="outlined-basic"
+              onChange={inputHandler}
+              variant="outlined"
+              fullWidth
+              label="Search"
+            />
+          </div>
+          <List input={inputText} />
+        </div>
       </header>
     </div>
   );
